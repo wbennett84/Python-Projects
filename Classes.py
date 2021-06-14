fbefba41d6c9df29a9e4372b0c209196b6ddc7ce
wbennett84 <81777304+wbennett84@users.noreply.github.com>
2021-06-14 18:27:58 -0400

    Commit Classes assignment

---
+++ Classes.py
@@ -0,0 +1,15 @@
+
+# Creates the first parent class
+class Human:
+    name = ' '
+    bloodtype = ' '
+
+# Creates a child class of Human
+    class Man(Human):
+        body = 'Masculine'
+        outfit = 'Jeans'
+        
+# Creates another child class of Human
+    class Woman(Human):
+        body = 'Feminine'
+        outfit = 'Dress'
