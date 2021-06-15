452421bcfcc5731c759babc97e8bc0076374673d
wbennett84 <81777304+wbennett84@users.noreply.github.com>
2021-06-15 11:55:17 -0400

    Commit Polymorphism assignment

---
+++ Classes and Methods.py
@@ -0,0 +1,75 @@
+
+
+
+class Car:
+    def __init__(self, color, top_speed):
+        self.color = color
+        self.top_speed = top_speed
+
+    
+
+    def information(self):
+        msg = ("You\'re about to drive a {} car with a top speed of {}MPH!!!".format(car1.color,car1.top_speed))
+        return msg
+
+    def survey(self):
+        question1 = input("You like cars? Y/N \n>>> ").lower()
+        if (question1) == ("y"):
+            print ("You definitely belong in the club!")
+        if (question1) == ("n"):
+            print ("You might not be interested in this subject!")
+
+
+class Body(Car):
+    def __init__(self, doors, bed):
+        self.doors = doors
+        self.bed = bed
+    def information(self):
+        msg = ("It has {} doors and {} bed...".format(body1.doors,body1.bed))
+        return msg
+
+    def survey(self):
+        question1 = input("You ready for the next line? Y/N \n>>> ").lower()
+        if (question1) == ("y"):
+            print ("Here it comes!")
+        if (question1) == ("n"):
+            print ("Then get ready!")
+
+
+
+
+class Brand(Car):
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    def information(self):
+        msg = ("And it\'s a {} {}!".format(cartype1.make,cartype1.model))
+        return msg
+
+    def survey(self):
+        question1 = input("Did you enter the car giveaway contest? Y/N \n>>> ").lower()
+        if (question1) == ("y"):
+            print ("Then you have won! Collect your new car just outside the doors!")
+        if (question1) == ("n"):
+            print ("I'm sorry, but someone else won the car")
+
+
+
+
+
+if __name__ == "__main__":
+    car1 = Car("Red", 155)
+    print (car1.information())
+    body1 = Body(2, "no")
+    print (body1.information())
+    cartype1 = Brand("Ford", "Mustang")
+    print (cartype1.information())
+    car1.survey()
+    body1.survey()
+    cartype1.survey()
+    
+    
+    
+    
+
+
